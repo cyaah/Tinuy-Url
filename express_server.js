2f39a0269286b1b81d3069fa61ebb250c0f831c0
Calvin Yah <calvin.yah@gmail.com>
2018-03-14 02:01:32 +0000

    Fixed bug that redirects to login page when trying to edit an URL

--- express_server.js
+++ express_server.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var cookieSession = require('cookie-session')
 var bcrypt = require('bcrypt');
@@ -15,7 +14,7 @@ app.use(cookieSession({
 
 // Create our own middleware to check for a logged in user!
 function checkUser(req, res, next) {
-  if (req.path.match(/login|register|delet|u\/./)) {
+  if (req.path.match(/login|register|u\/.delet|u\/./)) {
     next() // Execute next middleware or go to routes
     return
   }
@@ -37,12 +36,11 @@ app.use(checkUser)
 
 
 //Database to store Long URLS an their short urls
-var urlDatabase = {
-  //"user1" : {
+const urlDatabase = {
    "b2xVn2":
    {
    		longurl: "http://www.lighthouselabs.ca",
-   		userid : "user1"
+   		userid : "user2"
     },  
      "ca2Vn2":
    {
@@ -91,10 +89,7 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
 	let filterUrls = filterByUserId(urlDatabase,req.session.userid)
   let templateVars = { urls: filterUrls,
-  					   user: users[req.session.userid]}; // req.cookies["username"]}; || 'Anonymous' };
-  console.log('blah', templateVars);
-
-
+  					           user: users[req.session.userid]}; // req.cookies["username"]}; || 'Anonymous' };
   res.render("urls_index", templateVars);
 });
 
@@ -118,48 +113,49 @@ app.get("/urls", (req, res) => {
 
 
 app.get("/login",(req,res) =>{
-	res.render("urls_login");
-	
+  res.render("urls_login");
+  
 })
 
-
-
 //Creates a new random short url and sets it the submitted longurl
-app.post("/urls/new", (req, res) => {	
-	let shortURL = generateRandomString();
-	urlDatabase[shortURL] = {
-		"longurl" : req.body.longURL,
-		"userid"  : req.currentUser
-	}
-  console.log('blah', req.body.longURL);  
+app.post("/urls/new", (req, res) => { 
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    "longurl" : req.body.longURL,
+    "userid"  : req.currentUser
+  }
+  console.log('blah', urlDatabase[shortURL]);  
   res.redirect("/urls");      
+});
+//Adds a new link and generating a random short url
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = {
+   "longurl" : req.body.longURL,
+   "userid"  : req.currentUser
+  };
+	console.log("hi", urlDatabase[req.params.id] );
+    res.redirect("/urls");
 });
 
 //Redirects the page to the long url page when short url is used
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL].longurl
-  console.log(longURL);
-  res.redirect(longURL);
-});
-//Deleting the url
-app.post("/urls/:id/delet", (req, res) => {
-	delete urlDatabase[req.params.id];
-	res.redirect("/urls");
+  let db = urlDatabase[req.params.shortURL];
+  res.redirect(db.longurl);
+ // res.redirect(urlDatabase[req.params.shortURL].longurl);  
 });
 
-//Adds a new link and generating a random short url
-app.post("/urls/:id", (req, res) => {
-urlDatabase[req.params.id] = req.body.longURL;
-	console.log("jaa");
-    res.redirect("/urls");
+//Deleting the url
+app.post("/urls/:id/delet", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
 });
+
 
 //Sets the submitted string in a cookie and shows the user is logged in"
 app.post("/login",(req, res) => {
 	let email = req.body.email
 	let password = req.body.password
 	let hashedPassword = bcrypt.hashSync(password, 10);
-  console.log(hashedPassword);
 	let currentUser = req.body.currentUser	
   for (var uid in users) { 
     if(email === users[uid].email) { 
@@ -168,7 +164,7 @@ app.post("/login",(req, res) => {
         // set a cookie to keep track of the user
   	  	  req.session.userid = uid;
           res.redirect("/urls");
-   	  	  console.log("job");
+   	 
         } else {
   	      res.status(403).send("Email or Password incorrect")
          	console.log("403");
@@ -202,7 +198,7 @@ app.post("/register", (req, res) =>{
     
     else{
 	users[user] = {'email' : req.body.email, 'password' : hashedPassword }
-     console.log(users);
+     
 	res.redirect("/urls");
 	}
 
@@ -210,16 +206,16 @@ app.post("/register", (req, res) =>{
 
 
 function generateRandomString() {
-return Math.random().toString(16).substring(2,8) 
+  return Math.random().toString(16).substring(2,8) 
 }
 
 
 function filterByUserId (urls, userId){
-return Object.entries(urls).filter(entry=> {
-	console.log(userId);
-	console.log(entry[1].userid);
-	return entry[1].userid === userId
-	})
+ return Object.entries(urls).filter(entry=> {
+	//console.log(userId);
+	//console.log(entry[1].userid, "entry");
+   return entry[1].userid === userId
+	 })
     .reduce((acc,entry)=>{
     	let short = entry[0];
     	let content = entry [1];
